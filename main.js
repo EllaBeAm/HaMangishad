4158c723719d8b9412c6ded40925254d6559c969
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2022-05-22 22:22:05 +0300

    3 nav bars with animations

--- main.js
+++ main.js
@@ -12,7 +12,35 @@ function selectedDate(e) {
     selectedTypeBtn.classList.add("active")
 }
 
+function selectedSection(e) {
+    let selectedSectionBtn = e.target
+    let sectionButtons = document.querySelectorAll("button.section-btn")
+    sectionButtons.forEach(btn => btn.classList.remove("active"))
+    selectedSectionBtn.classList.add("active")
+}
+
 function shiftDateBtns(dir) {
     console.log(dir);
-    
+    let leftScrollDistance = 0
+    switch (dir) {
+        case "right":
+            leftScrollDistance = 100
+            break
+        case "left":
+            leftScrollDistance = -100
+            break
+    }
+    let dateBtnsWrapperElm = document.querySelector(".date-btns-wrapper")
+    dateBtnsWrapperElm.scrollBy({
+        top: 0,
+        left: leftScrollDistance,
+        behavior: 'smooth'
+    });
 }
+
+function init() {
+    let dateBtnsWrapperElm = document.querySelector(".date-btns-wrapper")
+    dateBtnsWrapperElm.scrollBy(-100, 0);
+}
+
+init()
