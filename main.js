c8736333d6861af07f27b192caf12f47029fec4b
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2022-05-22 18:35:01 +0300

    header & btns without dates scrolling

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hii");
+function selectedType(e) {
+    let selectedTypeBtn = e.target
+    let typeButtons = document.querySelectorAll("button.type-btn")
+    typeButtons.forEach(btn => btn.classList.remove("active"))
+    selectedTypeBtn.classList.add("active")
+}
+
+function selectedDate(e) {
+    let selectedTypeBtn = e.target
+    let typeButtons = document.querySelectorAll("button.date-btn")
+    typeButtons.forEach(btn => btn.classList.remove("active"))
+    selectedTypeBtn.classList.add("active")
+}
+
+function shiftDateBtns(dir) {
+    console.log(dir);
+    
+}
